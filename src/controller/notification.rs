5968e28670a9ce30816ec6ac7761c3d09f06d9bf
FBimo <f.bimo31@gmail.com>
2024-04-17 02:18:24 +0700

    fix subscriber variable name typo

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,12 +1,12 @@
 use rocket::response::status::Created;
-use rocket::serde::json::json;
+use rocket::serde::json::Json;
 
 use bambangshop::Result;
 use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
 
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
-pub fn subscribe(product_type: &str, susbcriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
+pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
     return match NotifcationService::subscribe(product_type, subscriber.into_inner()) {
         Ok(f) => Ok(Created::new("/").body(Json::from(f))),
         Err(e) => Err(e)
