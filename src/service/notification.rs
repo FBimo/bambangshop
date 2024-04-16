54376f0ea488e59783d29bc3191c219eaf985b62
FBimo <f.bimo31@gmail.com>
2024-04-17 02:49:18 +0700

    fix str data type

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -19,7 +19,7 @@ impl NotificationService {
 
     pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
         let product_type_upper: String = product_type.to_uppercase();
-        let product_type_str: String = product_type_upper.as_str();
+        let product_type_str: &str = product_type_upper.as_str();
         let result: Option<Subscriber> = SubscriberRepository::delete(product_type_str, url);
         if result.is_none() {
             return Err(compose_error_response(
