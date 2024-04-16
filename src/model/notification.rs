8f5d5d53005aeedd912cdca868c124edb58113e6
FBimo <f.bimo31@gmail.com>
2024-04-16 23:16:57 +0700

    Create Notification model struct

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -1,8 +1,8 @@
 use rocket::serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Deserialize,Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
-pub struct Notfication {
+pub struct Notification {
     pub product_title: String,
     pub product_type: String,
     pub product_url: String,
