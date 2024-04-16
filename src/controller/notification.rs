ebbfeb64acde9635746589997f4da670248c5c83
FBimo <f.bimo31@gmail.com>
2024-04-17 02:28:58 +0700

    fix NotificationService typo

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -7,7 +7,7 @@ use crate::service::notification::NotificationService;
 
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
 pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
-    return match NotifcationService::subscribe(product_type, subscriber.into_inner()) {
+    return match NotificationService::subscribe(product_type, subscriber.into_inner()) {
         Ok(f) => Ok(Created::new("/").body(Json::from(f))),
         Err(e) => Err(e)
     };
@@ -15,7 +15,7 @@ pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Cre
 
 #[post("/unsubscribe/<product_type?>?<url>")]
 pub fn unsubscribe(product_type: &str, url: &str) -> Result<Json<Subscriber>> {
-    return match NotifcationService::unsubscribe(product_type, url) {
+    return match NotificationService::unsubscribe(product_type, url) {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     };
